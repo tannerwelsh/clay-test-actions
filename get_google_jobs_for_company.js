788019586c58101c936c375e7e37d93b422aa38d
Tanner Welsh <tannerwelsh@gmail.com>
2020-04-28 12:59:10 -0600

    feature: Define action Get Google Jobs (mock data)
    For testing

---
+++ get_google_jobs_for_company.js
@@ -0,0 +1,183 @@
+const Clay = require('./clay_helper.js')
+
+const getGoogleJobsForCompanyDefinition = {
+  name: "mockgetgooglejobsforcompany",
+  function: mockGetGoogleJobsForCompany,
+  documentationUri: "http://github.com/clay-run/actions/myfirstactionguide.md",
+  displayName: "Get Google Jobs for Company",
+  description: "Mock action for finding jobs for a company using Google",
+  actionGroups: ["BI", "Jobs"],
+  inputParameterSchema: [
+    {
+      name: "companyName",
+      type: "text",
+      displayName: "Company Name",
+      description: "Name of company"
+    },
+    {
+      name: "companyURL",
+      type: "url",
+      displayName: "Company URL",
+      optional: true,
+      description: "URL of company (optional). Used instead of companyName"
+    },
+    {
+      name: "keywords",
+      displayName: "Keywords",
+      optional: true,
+      type: "text",
+      description: "Comma separated list of keywords"
+    }
+  ],
+  outputParameterSchema: [
+    {
+      name: "jobs",
+      type: "array"
+    },
+    {
+      name: "count",
+      type: "integer"
+    },
+    {
+      name: "hasMatches",
+      type: "boolean"
+    }
+  ],
+  inputSample: {
+    companyName: "Slack",
+    companyURL: "http://slack.com/",
+    keywords: "Data,Software",
+  },
+  outputSample: {
+    jobs: [
+      {
+        type: "Full-time",
+        title: "Data Engineer",
+        labels: {
+          location: "San Francisco, CA"
+        },
+        company: "Slack",
+        lastUpdated: "Over 1 month ago"
+      }, {
+        type: "Full-time",
+        title: "Staff Data Scientist",
+        labels: {
+          location: "San Francisco, CA"
+        },
+        company: "Slack",
+        lastUpdated: "Over 1 month ago"
+      }
+    ]
+  },
+  restrictionFlags: {
+    isExportable: true
+  },
+  isPublic: false,
+}
+
+const mockResults = [{
+  "type": "Full-time",
+  "title": "Data Engineer",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "Over 1 month ago"
+}, {
+  "type": "Full-time",
+  "title": "Staff Data Scientist",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "Over 1 month ago"
+}, {
+  "type": "Full-time",
+  "title": "Software Engineer, Data Infrastructure",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "Over 1 month ago"
+}, {
+  "type": "Full-time",
+  "title": "Sr. Data Scientist, Foundations",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "Over 1 month ago"
+}, {
+  "type": "Full-time",
+  "title": "Staff Data Scientist",
+  "labels": {
+    "location": "California"
+  },
+  "company": "Slack",
+  "lastUpdated": "6 days ago"
+}, {
+  "type": "Full-time",
+  "title": "Sr. Data Scientist, Enterprise",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "28 days ago"
+}, {
+  "type": "Full-time",
+  "title": "Sr. Data Engineer, Marketing",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "8 days ago"
+}, {
+  "type": "Full-time",
+  "title": "Sr. Data Scientist, Sales Analytics",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "Over 1 month ago"
+}, {
+  "type": "Full-time",
+  "title": "Sr. Product Manager, Data",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "Over 1 month ago"
+}, {
+  "type": "Full-time",
+  "title": "Sr. Data Engineer",
+  "labels": {
+    "location": "San Francisco, CA"
+  },
+  "company": "Slack",
+  "lastUpdated": "8 hours ago"
+}]
+
+function mockGetGoogleJobsForCompany(actionInputs, context) {
+  console.log("-- debug: in mockGetGoogleJobsForCompany() --")
+  console.log(" mockGetGoogleJobsForCompany -> actionInputs", actionInputs)
+  console.log(" mockGetGoogleJobsForCompany -> context", context)
+
+  if (actionInputs.companyName || actionInputs.companyURL) {
+    const textPreview = "10 matching jobs for Slack"
+    const imagePreview = "https://banner2.cleanpng.com/20180728/tju/kisspng-google-logo-business-microsoft-windows-operating-system-5b5cb99e99ca38.3321008115328034866299.jpg"
+
+    return Clay.success(
+      {
+        "jobs": mockResults,
+        "count": 10,
+        "hasMatches": true,
+      },
+      textPreview,
+      imagePreview,
+    )
+  }
+
+  return Clay.fail('Failed to run mockGetGoogleJobsForCompany()')
+}
+
+module.exports = getGoogleJobsForCompanyDefinition
---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ *
+ * This sample action package illustrates various rules and principles for action package creation
+ *
+ * An Action Package contains one or more Action Definitions
+ *
+ *
+ */
+
+const getGoogleJobsForCompany = require("./get_google_jobs_for_company.js")
+
+const myActionPackage = {
+  name: "tannerwelsh-test-actions", //to uniquely identify an action pacakge for future updates
+  description: "sample actions for development",
+  actionDefinitions: [getGoogleJobsForCompany],
+}
+
+module.exports = myActionPackage
