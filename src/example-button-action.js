9f2fa996410a64ee5808d5b2ee2a6dbc3aa38a74
Tanner Welsh <tannerwelsh@gmail.com>
2020-07-16 17:14:52 -0600

    add example button action

---
+++ src/example-button-action.js
@@ -0,0 +1,76 @@
+const _ = require('lodash');
+
+const exampleButtonActionDefinition = {
+  name: 'exampleButtonAction',
+  function: exampleButtonAction,
+  documentationUri: 'http://github.com/clay-run/actions/myfirstactionguide.md',
+  displayName: 'Example Button Action',
+  description: 'Mock action for showing a button column type',
+  actionGroups: [],
+  inputParameterSchema: [
+    {
+      name: 'email',
+      type: 'text',
+      displayName: 'Input email',
+      description: 'Email to send notification to',
+
+      // !!This disables automatic recomputes
+      skipRecomputeOnChange: true,
+    },
+  ],
+  outputParameterSchema: [
+    {
+      name: 'message',
+      type: 'text',
+    },
+  ],
+  inputSample: {
+    email: 'foo@example.com',
+  },
+  outputSample: {
+    message: 'it worked!',
+  },
+
+  // !!This is the key setting
+  viewOptions: {
+    button: {
+      showInCell: true,
+    },
+  },
+
+  restrictionFlags: {
+    isExportable: false,
+  },
+  isPublic: false,
+};
+
+async function exampleButtonAction(actionInputs, context) {
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      try {
+        if (!actionInputs.email) throw new Error('ERROR_MISSING_INPUT')
+        const data = {
+          message: `Notification sent to ${actionInputs.email}`,
+        };
+        const textPreview = 'Sent ✅';
+
+        resolve(
+          context.success({
+            data,
+            textPreview,
+          })
+        );
+      } catch (err) {
+        context.log(err.message);
+
+        reject(
+          context.fail({
+            message: `Action failed with error: ${err.message}`,
+          })
+        );
+      }
+    }, 2000);
+  });
+}
+
+module.exports = exampleButtonActionDefinition;
--- src/index.js
+++ src/index.js
@@ -7,15 +7,22 @@
  *
  */
 
-const getGoogleJobsForCompany = require("./get-google-jobs-for-company.js")
-const githubUserLookup = require("./github-user-lookup.js")
-const exampleDataShapes = require("./example-data-shapes.js")
-const exampleViewOptions = require("./example-view-options.js")
+const getGoogleJobsForCompany = require('./get-google-jobs-for-company.js');
+const githubUserLookup = require('./github-user-lookup.js');
+const exampleDataShapes = require('./example-data-shapes.js');
+const exampleViewOptions = require('./example-view-options.js');
+const exampleButtonAction = require('./example-button-action.js');
 
 const myActionPackage = {
-  name: "tannerwelsh_test_actions", //to uniquely identify an action pacakge for future updates
-  description: "sample actions for development",
-  actionDefinitions: [getGoogleJobsForCompany, githubUserLookup, exampleDataShapes, exampleViewOptions],
-}
+  name: 'tannerwelsh_test_actions', //to uniquely identify an action pacakge for future updates
+  description: 'sample actions for development',
+  actionDefinitions: [
+    getGoogleJobsForCompany,
+    githubUserLookup,
+    exampleDataShapes,
+    exampleViewOptions,
+    exampleButtonAction,
+  ],
+};
 
-module.exports = myActionPackage
+module.exports = myActionPackage;
